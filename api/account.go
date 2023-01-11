ea729096713ca2bc98b1971e43825b2199b82583
LeGion13 <legionbsd13@gmail.com>
2023-01-11 00:10:34 -0800

    fixed getAccount

--- api/account.go
+++ api/account.go
@@ -42,7 +42,6 @@ type getAccountRequest struct {
 }
 
 func (server *Server) getAccount(ctx *gin.Context) {
-
 	var req getAccountRequest
 	if err := ctx.ShouldBindUri(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
